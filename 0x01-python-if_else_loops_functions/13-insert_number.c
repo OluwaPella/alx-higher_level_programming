602216730f2badc20195c094bfc0be5b5d1a178f
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-05-29 08:02:10 -0700

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,26 +7,30 @@
  *@head: double pointer variable
  *@number:variable that print sorted singly linked list
  *Return: the address of the new node, or NULL if it failed
+ *otherwise - a pointer to the new node.
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *insert_node = *head , *new;
+	listint_t *node = *head, *new;
+
 	new = malloc(sizeof(listint_t));
-	if (new = NULL)
+	if (new == NULL)
 		return (NULL);
-	new->n =number;
-	 if (node == NULL || node->n >= number)
-    {
-        new->next = node;
-        *head = new;
-        return (new);
-    }
-	    while (node && node->next && node->next->n < number)
-        node = node->next;
+	new->n = number;
 
-    new->next = node->next;
-    node->next = new;
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
 
-    return (new);
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
 }
