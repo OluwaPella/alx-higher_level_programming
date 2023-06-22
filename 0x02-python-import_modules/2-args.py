e010e56bcf96fa1000099d29b50ac407521264d4
olaosebikan pelumi <oolaosebikan360@gmail.com>
2023-06-22 08:21:32 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argument = len(sys.argv) -1
+    argument = len(sys.argv) - 1
     if argument == 0:
         print("0 arguments.")
     elif argument == 1:
         print("1 arguments:")
     else:
         print("{} arguments:".format(argument))
-        for i in range(argument):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
+    for i in range(argument):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
