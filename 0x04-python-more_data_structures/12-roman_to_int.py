6661238221b8ffea1f52de08d099f18f6236919a
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-07-28 15:28:04 -0700

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    numerals = {'I':1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    for i in range(len(roman_string)):
+        value = numerals[roman_string[i]]
+        if i + 1 < len(roman_string) and numerals[roman_string[i + 1]] > value:
+            total -= value
+        else:
+            total += value
+    return total
