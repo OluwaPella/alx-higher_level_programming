609beebddf1ab2be508a469671f1b70fed3e96c6
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-07-29 05:02:11 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
+#!/usr/biiin/python3
 def roman_to_int(roman_string):
     numerals = {'I':1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not roman_string or type(roman_string) != str:
+        return 0
     total = 0
     for i in range(len(roman_string)):
         value = numerals[roman_string[i]]
