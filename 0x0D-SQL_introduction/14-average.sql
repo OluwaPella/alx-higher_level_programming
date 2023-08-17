ff0ba4920572e3adc07c0769dda729ca1d5bd011
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-17 04:42:37 -0700

    14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,6 +1,5 @@
 -- this script computes the score average of all records in the table second_table
 -- The result column name should be average
-
-SELECT AVG(score)
-WITH score as average
-FROM second_table;
+WITH average as(
+SELECT AVG(score) as average
+FROM second_table);
