7621b7c1fee1ff68aecc609c76b4395c80d1d6d7
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-17 01:56:31 -0700

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
- _ _ this script create table and set id and name
-CREATE TABLE IF NOT EXISTS first_table
+ _ _ this script create tables 
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
