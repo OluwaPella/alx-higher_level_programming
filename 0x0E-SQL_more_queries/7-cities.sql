9b73d4d5d6fb9eab3d4537f74295ed5b917afa0f
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-23 08:42:00 -0700

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,14 +1,11 @@
 -- Create the database if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Switch to using the newly created database
-USE hbtn_0d_usa;
-
--- Create the 'cities' table with a foreign key constraint
-CREATE TABLE IF NOT EXISTS cities (
+-- Create the 'cities' table within the 'hbtn_0d_usa' database
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES states (id)
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states (id)
 );
