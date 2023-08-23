a27072abb52bc8cfc3aa78f6f39dac96b39f50cb
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-23 05:25:38 -0700

    sql

--- 0x0E-SQL_more_queries/6-states.sql
+++
@@ -1,8 +0,0 @@
--- This script creates database and table in a database hbtn_0d_usa
-
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-    PRIMARY KEY(`id`),
-    `id`   INT          NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL
-);
