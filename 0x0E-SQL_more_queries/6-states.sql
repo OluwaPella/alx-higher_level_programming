ba0cf77b870109e769eb27031bff58797a269f6e
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-23 07:55:58 -0700

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,9 @@
 
 CREATE DATATBASE IF NOT EXISTS  hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states(
 	id INT NOT NULL AUTO_INCREMENT,
        	PRIMARY KEY(id),
 	name VARCHAR(256) NOT NULL);
