9ce6835d8b155023b5381273573328ec21456131
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-23 07:18:43 -0700

    3-force_name.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- this script creates the table force_name on your MySQL server
+-- If the table force_name already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS force_name(
+	id INT,
+	name VARCHAR(256) NOT NULL);
