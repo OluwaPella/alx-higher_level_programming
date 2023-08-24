e062762b43b61ba2390bc8e247a29082155276e9
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-24 05:02:45 -0700

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
---- this script lists all the cities of California that can be found in the database hbtn_0d_usa
+-- this script lists all the cities of California 
+-- that can be found in the database hbtn_0d_usa
 
 SELECT id, name
 FROM cities
