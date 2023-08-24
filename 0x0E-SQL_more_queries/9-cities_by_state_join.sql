02f2e9e2343d3878bcdbde82ded06740505a86b9
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-24 08:24:52 -0700

    cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- This script lists all cities contained in the database
 
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
-FROM  cities 
-INNER JOIN states
+SELECT cities.id, cities.name, states.name
+FROM  cities AS city_name
+INNER JOIN states AS state_name
 ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+ORDER BY cities.id;
