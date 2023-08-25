b9912cf6402ed4414e7bc47f7b8338d641b5ef3c
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-25 04:40:58 -0700

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
 -- script that lists all Comedy shows in the database
 
-SELECT t. title
+SELECT t.title
 FROM tv_shows AS t
 INNER JOIN tv_show_genres AS s
-ON g.id = s.genre_id
+ON t.id = s.show_id
 INNER JOIN tv_genre AS g
 ON g.id = s.genre_id
 WHERE g.name = "Comedy"
