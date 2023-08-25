459a6db678fa295f6d4ddf97da199e06fdd21844
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-25 03:43:28 -0700

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,7 +5,7 @@ SELECT genres AS genre,
 COUNT(*) AS number_of_shows
 FROM hbtn_0d_tvshows
 INNER JOIN tv_show_genres
-ON hbtn_0d_tvshows.genre_id = tv_show_genres.genre_id
+ON genre.id = tv_show_genres.genre_id
 GROUP BY  genre
 ORDER BY number_of_shows DESC;
 
