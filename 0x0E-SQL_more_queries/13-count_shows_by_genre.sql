3759e48e5a8c1a0515243177b681ce67e9303f8c
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-25 03:21:47 -0700

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,10 @@
 -- this script count numbers of genres
 
-SELECT genres AS g, count(*) AS number_of_shows
+SELECT genres AS g, COUNT(*) AS number_of_shows
 FROM hbtn_0d_tvshows
 INNER JOIN tv_show_genres ON hbtn_0d_tvshows.genre_id = tv_show_genres.genre_id
 GROUP BY  genre
+HAVING
+    COUNT(*) > 0
 ORDER BY number_of_shows DESC;
 
