6ebbd633dbda4fd353e9cace02212cba14672d84
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-09-13 01:55:42 +0000

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 /* this  script print ints 3 lines
 */
-
 const good = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < good.length; i++){
-console.log(good[i]);
+for (let i = 0; i < good.length; i++) {
+  console.log(good[i]);
 }
