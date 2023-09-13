1edf50a4c9b5dfc60dd4e595bc39ee65e3b7965a
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-09-13 11:41:00 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 /* This script print x once it given args
 */
-
 const num = Math.floor(Number(process.argv[2]));
 if (!isNaN(num)) {
-for (let i = 0; i < num; i++){
-  console.log('C is fun');
-}
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 } else {
   console.log('Missing number of occurrences');
 }
