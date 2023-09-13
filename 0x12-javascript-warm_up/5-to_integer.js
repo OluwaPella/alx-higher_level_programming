f59c699dd08b4a2826c8037f70137f9a128f0416
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-09-13 01:27:47 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 /* this print number
 */
-if (parseInt(process.argv.length)) {
-  console.log(`My number: ${process.argv[2]}`);
+const num = Math.floor(Number(process.argv[2]));
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
 } else {
   console.log('Not a number');
-}  
+}
