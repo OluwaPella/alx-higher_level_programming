b215fe45d85f485f7eb43ff09d5702768bdbc2bb
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-09-13 13:06:58 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,13 +4,13 @@
 const size = Math.floor(Number(process.argv[2]));
 
 if (!isNaN(size)) {
-    for (let i = 0; i < size; i++) {
-      let row = '';
-      for (let j = 0; j < size; j++) {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
         row += 'X';
-      }
-      console.log(row);
     }
-  } else {
+    console.log(row);
+  }
+} else {
   console.log('Missing size');
 }
