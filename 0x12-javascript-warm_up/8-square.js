20fb0e89b63006480c0d657bede1b6a523ac387c
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-09-13 12:59:11 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
-/* this script print x with the number of argument
+/* this script print x with the number of argumnet
 */
+const size = Math.floor(Number(process.argv[2]));
 
-const num = Math.floor(Number(process.argv[2]));
-
-if (!isNaN(num)) {
-if (num > 0) {
-  for (let i = 0; i < num; i++) {
-    let row  = '';
-    for (let n = 0; n < num; n++) {
-      row += 'x';
+if (!isNaN(size)) {
+  if (size > 0) {
+    for (let i = 0; i < size; i++) {
+      let row = '';
+      for (let j = 0; j < size; j++) {
+        row += 'X';
+      }
+      console.log(row);
     }
-    console.log(row);
+  } else {
+    console.log('Size must be a positive number');
   }
-}  
-}else {
+} else {
   console.log('Missing size');
 }
